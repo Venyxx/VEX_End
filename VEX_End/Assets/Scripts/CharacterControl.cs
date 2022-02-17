543c1831b36c859d3379f179797252265e9d7bd8
Carlos Rojas <funkieos@knights.ucf.edu>
2022-02-17 01:11:30 -0500

    Completed Player
    -Imported Idle & run animation
    -Created blend trees
    -Scripted WASD keys for animation

--- VEX_End/Assets/Scripts/CharacterControl.cs
+++ VEX_End/Assets/Scripts/CharacterControl.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)){
+            anim.SetBool("isRunning", true);
+        } else {
+            anim.SetBool("isRunning", false);
+        }
     }
 }

903875c1e031f0045793fbfdb36e71148431ecdf
Carlos Rojas <funkieos@knights.ucf.edu>
2022-02-20 19:19:26 -0500

    ArtTest
    Created Unity scene created a museum-like scene

---
+++ VEX_End/Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)){
+            anim.SetBool("isRunning", true);
+        } else {
+            anim.SetBool("isRunning", false);
+        }
+    }
+}
